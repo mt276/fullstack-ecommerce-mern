984e3eea6b25cf24037841658844fcc91532a5a1
Duong Minh Trieu <trieu7458@gmail.com>
2025-02-27 16:48:05 +0700

    client-header

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home.js";
+import Header from "./components/Header/index.js";
 
 function App() {
   return (
     <>
-      <h1 className="text-danger">Ecommerce website in react js</h1>
+      <BrowserRouter>
+        <Header>
+          <Routes>
+            <Route path="/" exact={true} Component={<Home />} />
+          </Routes>
+        </Header>
+      </BrowserRouter>
     </>
   );
 }
---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <>
+      <h1>Ecommerce website in react js</h1>
+    </>
+  );
+};
+
+export default Home;
